d8b0f8d534ebf22ce7ad2561684a1989e97f3715
David Dashifen Kees <dashifen@dashifen.com>
2023-05-09 14:38:30 -0400

    index.php with plugin header

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Movies (Plugin Development Example)
+ * Description: An example of WordPress plugin development focusing on a custom post type for movies.
+ * Author URI: https://dashifen.com
+ * Author: David Dashifen Kees
+ * Version: 1.0.0
+ *
+ * For full Plugin Header options, see:
+ * https://developer.wordpress.org/plugins/plugin-basics/header-requirements/
+ */
+
+if (!defined('ABSPATH')) {
+  exit;
+}
